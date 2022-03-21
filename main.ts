0e5be33bae28cde4152f57df0e36aef753fe4ba5
KrazyManJ <KrazyManJ@gmail.com>
2022-03-21 16:28:41 +0100

    Fixed bugs, added all global options, added link optimalization via checking if value was changed, enhanced link formatter

--- main.ts
+++ main.ts
@@ -4,14 +4,27 @@ window.onload = () => {
 	var input = document.querySelector("form");
 	var preview = document.getElementById("preview")
 
-	Array.from(input.elements).forEach(e => {
-		e.value = e.getAttribute("placeholder")
-	});
+	
+	document.querySelectorAll('form > input:not([name="username"])').forEach(f => {
+		f.setAttribute("changed", "false")
+		f.addEventListener("input", () => {
+			if (f.getAttribute("changed") === "false") f.setAttribute("changed","true")
+		})
+	})
 
 	updatePreview()
 	
 	document.getElementById("input").addEventListener("change", () => {
 		updatePreview()
+		
+		// Code for adding "are you sure to leave?"
+		//
+		// window.onbeforeunload = (e) => {
+		// 	e = e || window.event
+		// 	var message = null
+		// 	if (e) { e.returnValue = message }
+		// 	return message
+		// }
 	})
 
 
@@ -21,9 +34,22 @@ window.onload = () => {
 
 	
 		Array.from(input.elements).forEach(e => {
-			params.push(`${e.name}=${e.value}`)
+			if (e.getAttribute("changed") != "false") switch (e.getAttribute("type")) {
+				case "color": 
+					params.push(`${e.getAttribute("name")}=${e.value.replace("#","")}`)
+					break;
+				case "checkbox":
+					params.push(`${e.name}=${e.value == "on" ? "true" : "false"}`)
+					break;
+				case "text":
+					if (e.value.length > 0) params.push(`${e.name}=${e.value}`)
+					break;
+				default: 
+					params.push(`${e.name}=${e.value}`)
+					break
+				
+			}
 		});
-	
-		preview.src = link+params.join("&")
+		preview.setAttribute("src",link+params.join("&"))
 	}
 }
