dd2bc97fc584ddc365b262a651f789a70b87f39e
KrazyManJ <KrazyManJ@gmail.com>
2022-03-21 07:27:07 +0100

    Added basic properties to edit on profile card, updating, etc.

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 "use strict";
-var test = "";
+var link = "https://github-readme-stats.vercel.app/api?";
+window.onload = () => {
+    var input = document.querySelector("form");
+    var preview = document.getElementById("preview");
+    Array.from(input.elements).forEach(e => {
+        e.value = e.getAttribute("placeholder");
+    });
+    updatePreview();
+    document.getElementById("input").addEventListener("change", () => {
+        updatePreview();
+    });
+    function updatePreview() {
+        var params = [];
+        Array.from(input.elements).forEach(e => {
+            params.push(`${e.name}=${e.value}`);
+        });
+        preview.src = link + params.join("&");
+    }
+};
--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-var test = ""
+var link = "https://github-readme-stats.vercel.app/api?"
+
+window.onload = () => {
+	var input = document.querySelector("form");
+	var preview = document.getElementById("preview")
+
+	Array.from(input.elements).forEach(e => {
+		e.value = e.getAttribute("placeholder")
+	});
+
+	updatePreview()
+	
+	document.getElementById("input").addEventListener("change", () => {
+		updatePreview()
+	})
+
+
+	function updatePreview() {
+		
+		var params:string[] = [];
+
+	
+		Array.from(input.elements).forEach(e => {
+			params.push(`${e.name}=${e.value}`)
+		});
+	
+		preview.src = link+params.join("&")
+	}
+}
