3db9d6f00fa09d9c359f845506df34322c772991
KrazyManJ <KrazyManJ@gmail.com>
2022-04-02 19:02:05 +0200

    Added functionality to Auto update feature

--- main.js
+++ main.js
@@ -43,15 +43,25 @@ window.onload = () => {
 	const card_type_container = document.getElementById("card_type")
 	const username_container = document.getElementById("username")
 	const excl_opt_container = document.getElementById("exl_opt_container")
+	const auto_update_container = document.getElementById("auto-update")
 	//other stuff
 	const preview_img = document.getElementById("preview")
 	const copy_button = document.getElementById("copy_button")
+	const update_button = document.getElementById("update_button")
+
+	auto_update_container.onchange = () => {
+		console.log("test", update_button.parentElement.hidden, auto_update_container.checked)
+		update_button.parentElement.hidden = auto_update_container.checked
+	}
+	update_button.onclick = () => {
+		updatePreview()
+	}
 
 
 	card_type_container.onchange = () => {
 		preview_img.className = card_type_container.value
 		updateExtraOptions()
-		updatePreview()
+		if (auto_update_container.checked) updatePreview()
 	}
 
 	username_container.onchange = () => updatePreview()
@@ -59,7 +69,7 @@ window.onload = () => {
 		if (!["username","repository","auto-update"].includes(i.name)) {
 			i.onchange = () => {
 				gen_data[i.name] = formatValue(i)
-				updatePreview()
+				if (auto_update_container.checked) updatePreview()
 			}
 		}
 	})
@@ -81,12 +91,6 @@ window.onload = () => {
 		updateExtraOptions()
 		preview_img.className = card_type_container.value
 	}
-
-
-
-
-
-
 	function updatePreview() {
 		var args = ""
 		for (var key in gen_data) args += "&"+key+"="+encodeURIComponent(gen_data[key])
@@ -106,11 +110,10 @@ window.onload = () => {
 		excl_opt_container.querySelectorAll("input,select").forEach(i => {
 			i.onchange = () => {
 				exl_data[i.name] = formatValue(i)
-				updatePreview()
+				if (auto_update_container.checked) updatePreview()
 			}
 		})
 	}
-
 	function formatValue(elem) {
 		if (elem.type === "color") return elem.value.replace("#", "")
 		else if (elem.type === "checkbox") return elem.checked
@@ -129,13 +132,6 @@ function wrapInput(type, name, label, defaultvalue, options) {
 	</div>
 	`
 }
-/**
- * 
- * @param {string} name 
- * @param {string} label 
- * @param {Array} values 
- * @returns 
- */
 function wrapSelect(name, label, values) {
 	var options = ""
 	values.forEach(v => {
